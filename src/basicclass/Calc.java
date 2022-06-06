3b990777d862390ab55ef36ee319fc21f451848d
Akshay Jaiswal <akshayjaiswalr@gmail.com>
2022-06-06 12:11:56 +0530

    Adds calc.java | Akshay

--- src/basicclass/Calc.java
+++ src/basicclass/Calc.java
@@ -2,10 +2,22 @@ package basicclass;
 import java.util.*;
 
 public class Calc {
-
+					
+			public int add(int i,int j)
+			{
+				int sum = i+j;	
+				return sum;
+			}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+							Calc c = new Calc();
+							Scanner sc=new Scanner(System.in);
+							System.out.print("Enter First number:");
+							int i = sc.nextInt();
+							System.out.print("Enter Second number:");
+							int j = sc.nextInt();
+							int sum= c.add(i,j);
+							System.out.print("Sum of two numbers:"+sum);		
+							}
 
 }
